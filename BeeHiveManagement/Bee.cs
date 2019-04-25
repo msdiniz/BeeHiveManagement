3114e249ff029115e8c7534e7901b6a256a270b2
Marcelo Scofano Diniz <marceloscofano@integraremed.com>
2019-04-24 22:25:43 -0300

    Adicionar arquivos de projeto.

---
+++ BeeHiveManagement/Bee.cs
@@ -0,0 +1,19 @@
+﻿namespace BeeHiveManagement
+{
+    class Bee
+    {
+        public const double HoneyUnitsConsumedPerMg = .25;
+
+        public double WeightMg { get; private set; }
+
+        public Bee(double weightMg)
+        {
+            WeightMg = weightMg;
+        }
+
+        virtual public double HoneyConsumptionRate()
+        {
+            return WeightMg * HoneyUnitsConsumedPerMg;
+        }
+    }
+}

a85b4eb924f4543ac28807b872ee9e65ebe813e7
Marcelo Scofano Diniz <marceloscofano@integraremed.com>
2019-04-25 00:06:42 -0300

    Trying to implement NectarStinger

--- BeeHiveManagement/Bee.cs
+++ BeeHiveManagement/Bee.cs
@@ -1,10 +1,32 @@
 ﻿namespace BeeHiveManagement
 {
-    class Bee
+    class Bee : IStingPatrol
     {
         public const double HoneyUnitsConsumedPerMg = .25;
 
         public double WeightMg { get; private set; }
+
+        private int alertLevel = 5; // { get ; }
+
+        public int AlertLevel 
+        {
+            get {
+                return alertLevel; 
+                }
+        }
+
+        private int stingerLength;
+        public int StingerLength { get => stingerLength; set => stingerLength = value; }
+        
+        public bool LookForEnemies()
+        {
+            return true;
+        }
+
+        public int SharpenStinger(int length)
+        {
+            return length;
+        }
 
         public Bee(double weightMg)
         {
---
+++ BeeHiveManagement/NectarStinger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeHiveManagement
+{
+    class NectarStinger : Worker, INectarCollector,
+    IStingPatrol
+    {
+
+        public int Nectar { get; set; }
+
+        public void FindFlowers() {
+        }
+        public void GatherNectar() {
+        }
+        public void ReturnToHive() {
+
+        }
+    }
+}
